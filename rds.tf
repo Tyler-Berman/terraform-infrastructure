ef9a388232b59a670c18c9b1bd7d751dcb5316bb
unknown <tyrox13@gmail.com>
2026-04-15 00:05:57 -0400

    Created a simple RDS database using Terraform

---
+++ rds.tf
@@ -0,0 +1,12 @@
+resource "aws_db_instance" "MyRDS" {
+    identifier = "first-rds"
+    instance_class = "db.t2.micro"
+    engine = "mariadb"
+    engine_version = "10.2.21"
+    username = ""
+    password = ""
+    port = 3306
+    allocated_storage = 20
+    skip_final_snapshot = true
+    db_name = "My-First-RDS"
+}
